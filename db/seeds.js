3e47889dbe199a424e2933299d555afbf9b79923
Walter Goins <wgoinz@gmail.com>
2019-01-08 13:17:14 -0500

    finished seeds.js file

--- db/seeds.js
+++ db/seeds.js
@@ -2,42 +2,63 @@ const Character = require('../models/Character')
 const Profile = require('../models/Profile')
 const Stats = require('../models/Stats')
 
-Profile.deleteMany({})
-    .then(() => {
-        return Profile.create({
-            username: "Ryu",
-            profilePic: "https://imgur.com/gallery/EuDyMPK",
-            slogan: "The answer lies in the heart of battle",
-            email: "ryu@capcom.com",
-            characters: []
-        }).then((profile) => {
-            // console.log("this is the " + profile)
-            const newChar = Character.create({
-                name: "Ibuki",
-                archetype: "Offensive",
-                stamina: 950,
-                stats: []
-            }).then((character) => {
-                profile.characters.push(character)
-                // console.log("profile is now" + profile)
-            }).then((completeProfile) => {
-                // console.log(profile)
-                const newStat = Stats.create({
-                    tournamentName: "Kumite in Tennessee",
-                    wins: 5,
-                    losses: 1,
-                    matches: 6,
-                    victoryPercentage: 83
+Stats.deleteMany({}).then(() => {
+    Character.deleteMany({}).then(() => {
+        Profile.deleteMany({})
+            .then(() => {
+                return Profile.create({
+                    username: "Ryu",
+                    profilePic: "https://imgur.com/gallery/EuDyMPK",
+                    slogan: "The answer lies in the heart of battle",
+                    email: "ryu@capcom.com",
+                    characters: []
+                }).then((profile) => {
+                    const newChar = Character.create({
+                        name: "Ibuki",
+                        archetype: "Offensive",
+                        stamina: 950,
+                        stats: []
+                    }).then((character) => {
+                        profile.characters.push(character)
+
+                    })
+                    const newChar2 = Character.create({
+                        name: "Ryu",
+                        archetype: "Balanced",
+                        stamina: 1000,
+                        stats: []
+                    }).then((character) => {
+                        profile.characters.push(character)
+                    }).then((stat) => {
+                        const newStat = Stats.create({
+                            tournamentName: "Kumite in Tennessee",
+                            wins: 5,
+                            losses: 1,
+                            matches: 6,
+                            victoryPercentage: 83
+                        }).then((stat) => {
+                            profile.characters[0].stats.push(stat)
+                            // console.log(profile)
+                        })
+
+                        const newStat2 = Stats.create({
+                            tournamentName: "Kumite in Tennessee",
+                            wins: 5,
+                            losses: 5,
+                            matches: 10,
+                            victoryPercentage: 50
+                        }).then((stat) => {
+                            profile.characters[1].stats.push(stat)
+                            // console.log(profile)
+                        })
+                        Promise.all([newChar, newStat], [newChar2, newStat2])
+                            .then(() => {
+                                profile.save()
+                                console.log(profile)
+                            })
+                    })
                 })
-            }).then((stat) => {
-                // console.log(profile)
-                profile.characters[0].stats.push(stat)
-                console.log(profile)
             })
 
-            Promise.resolve(newChar)
-                .then(() => {
-                    profile.save()
-                })
-        })
     })
+})
